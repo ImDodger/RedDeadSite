3bf432b4e18f9fbd4ff67fbb34749e9d91338e74
Matthew Cleaver <matth3w.cleaver@gmail.com>
2021-03-23 16:24:02 -0400

    Added button component.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,18 @@ import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
 
+const STYLES = ['btnPrimary', 'btnOutline']
+
+const SIZES = ['btnMedium', 'btnLarge']
+
 export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
-    const
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+    return (
+        <button className={`btn ${checkButtonStyle} ${checkButtonSize}`, onClick={onClick}, type=(type)}>
+            {children}
+        </button>
+    )
 }
